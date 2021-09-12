6a545b362c481449ba3e77733e46e10d1c6bc2de
Ivan Guimera <ivanguimera93@gmail.com>
2021-09-12 17:42:25 +0200

    angular app

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,29 @@
+
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientModule} from '@angular/common/http';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports : [RouterTestingModule,HttpClientModule],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+  it(`should have as title 'angular-test'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('angular-test');
+  });
+
+  
+});
