41989c4da6d965d8e7e7bbd032db0b5d814cb584
iguimera <90245613+iguimera@users.noreply.github.com>
2021-09-13 10:00:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-login'));
+app.use(express.static('./dist/angular-registration-login-example'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-login/'}),
+    res.sendFile('index.html', {root: 'dist/angular-registration-login-example/'}),
 );
 
 // Start the app by listening on the default Heroku port
