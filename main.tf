b30e934ab8dd110367196ecdba8d822268fd3fa6
iguimera <90245613+iguimera@users.noreply.github.com>
2021-09-14 12:25:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,8 +9,7 @@ terraform {
 }
 
 provider "heroku" {
-	email = "ivanguimera93@gmail.com"
-	api_key = "cef639ff-1092-4ec6-bee0-af562198391d"
+	
 }
 
 resource "heroku_app" "example" {
@@ -18,15 +17,6 @@ resource "heroku_app" "example" {
   region = "us"
 }
 
-# Build code & release to the app
-resource "heroku_build" "example" {
-  app        = heroku_app.example.name
-
-  source {
-    url     = "https://github.com/iguimera/angular-test/archive/refs/tags/v1.0.0.tar.gz"
-    version = "1.0.0"
-  }
-}
 
 output "example_app_url" {
   value = "https://${heroku_app.example.name}.herokuapp.com"
