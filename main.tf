cd7661cf7edb4727c2643d8eade2bd81709aa173
iguimera <90245613+iguimera@users.noreply.github.com>
2021-09-14 18:35:42 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,8 @@ terraform {
 }
 
 resource "heroku_app" "example" {
+  email = "ivanguimera93@gmail.com"
+	api_key = "cef639ff-1092-4ec6-bee0-af562198391d"
   name   = "easy-go-beeed"
   region = "us"
 }
