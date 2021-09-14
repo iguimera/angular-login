59966a95e41f618068f42eebdbc7dd3272b5a2ea
iguimera <90245613+iguimera@users.noreply.github.com>
2021-09-14 16:22:53 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
 # Terraform HCL
 terraform {
-  required_providers {
-    heroku = {
-      source  = "heroku/heroku"
-      version = "~> 4.0"
-    }
-  }
-  
   backend "remote" {
     organization = "easyGOband"
 
@@ -14,19 +7,4 @@ terraform {
       name = "github-actions-test"
     }
   }
-}
-
-provider "heroku" {
-	email = "ivanguimera93@gmail.com"
-	api_key = "cef639ff-1092-4ec6-bee0-af562198391d"
-}
-
-resource "heroku_app" "example" {
-  name   = "easy-go-bute"
-  region = "us"
-}
-
-
-output "example_app_url" {
-  value = "https://${heroku_app.example.name}.herokuapp.com"
 }
