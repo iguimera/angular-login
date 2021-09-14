22fac6e1038170c77502438279e24a81669b67a8
iguimera <90245613+iguimera@users.noreply.github.com>
2021-09-14 17:00:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/heroku"
-      version = "3.26.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.1"
-    }
-  }
-  required_version = ">= 0.14"
-
   backend "remote" {
     organization = "easyGOband"
 
@@ -21,24 +9,9 @@ terraform {
 }
 
 
-provider "aws" {
-  region = "us"
-}
-
-
-
-resource "random_pet" "sg" {}
-
 resource "heroku_app" "web" {
-  name                    = "iep"
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
+  name      = "iep"
+  region    = "us"
 }
 
 
-output "web-address" {
-  value = "${heroku_app.web.name}:8080"
-}
