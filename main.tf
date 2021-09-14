0116e2b06569497795ea6f318ad3ea8b6d38f8ab
iguimera <90245613+iguimera@users.noreply.github.com>
2021-09-14 16:40:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,17 @@ terraform {
   
   required_providers {
     heroku = {
-      source  = "heroku/heroku"
+      source  = "hashicorp/heroku"
       version = "~> 4.0"
     }
   }
+}
+
+resource "heroku_app" "example" {
+  name   = "hola"
+  region = "us"
+}
+
+output "example_app_url" {
+  value = "https://${heroku_app.example.name}.herokuapp.com"
 }
