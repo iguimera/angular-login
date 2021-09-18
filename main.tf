f310b135332327435f2a9f3c979b9ed7c491daa2
iguimera <90245613+iguimera@users.noreply.github.com>
2021-09-18 10:49:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,12 +17,29 @@ terraform {
 }
 
 variable "branch" {
-  description = "hoaldwad"
+  description = "Name of the Heroku app"
+}
+
+variable "source" {
+  description = "Name of the source release"
 }
 
 resource "heroku_app" "example" {
   name   = var.branch
   region = "us"
+}
+
+# Build code & release to the app
+resource "heroku_build" "example" {
+  app        = heroku_app.example.name
+
+  source {
+    url     = var.source
+  }
+}
+
+output "example_app_url" {
+  value = "https://${heroku_app.example.name}.herokuapp.com"
 }
 
 
