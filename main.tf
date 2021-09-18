68cbd65544a86e867d422a5333d40e9a97440480
iguimera <90245613+iguimera@users.noreply.github.com>
2021-09-18 12:19:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "heroku_build" "example" {
   app = heroku_app.example.name
 
   source {
-    url     = "https://github.com/iguimera/angular-login/archive/refs/tags/test.tar.gz")
+    url     = "https://github.com/iguimera/angular-login/archive/refs/tags/test.tar.gz"
     version = "1.0.0"
   }
 }
