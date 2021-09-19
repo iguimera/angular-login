9715987368d5323dbed1cd9c24567db7ee41d2e5
iguimera <90245613+iguimera@users.noreply.github.com>
2021-09-19 12:16:36 +0200

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    heroku = {
-      source = "heroku/heroku"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-  }
-  backend "remote" {
-    organization = "easyGOband"
-
-    workspaces {
-      name = "github-actions-test"
-    }
-  }
-}
 
 variable "branch" {
   description = "Name of the Heroku app"
