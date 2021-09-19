3b112d066967aef51f37bb70d94b64d749f2ba23
iguimera <90245613+iguimera@users.noreply.github.com>
2021-09-19 12:12:33 +0200

    Create main.tf

---
+++ modules/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    heroku = {
+      source = "heroku/heroku"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+  }
+  backend "remote" {
+    organization = "easyGOband"
+
+    workspaces {
+      name = "github-actions-test"
+    }
+  }
+}
+
+variable "branch" {
+  description = "Name of the Heroku app"
+}
+
+
+resource "heroku_app" "example" {
+  name   = "easygoband-${var.branch}"
+  region = "us"
+}
+
+
+resource "heroku_build" "example" {
+  app = heroku_app.example.name
+
+  source {
+    url     = "https://github.com/iguimera/angular-login/archive/refs/tags/${var.branch}.tar.gz"
+    version = "1.0.0"
+  }
+}
